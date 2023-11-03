85d543afe8efa8f2dc673d3072647b655a8873ea
marto <67065789+Marte77@users.noreply.github.com>
2023-11-03 23:57:01 +0000

    Update create_list.py

--- create_list.py
+++ create_list.py
@@ -4,12 +4,16 @@ blog_folder = 'blog-md'
 os.chdir(blog_folder)
 try:
     os.remove(file_list_name)
+    print('removed file')
 except FileNotFoundError: 
     pass
 file_list = open(file_list_name, "w")
+print('created file')
 files = os.listdir('list')
 for index, f in enumerate(files):
     file_list.write(f)
     if index +1 != len(files):
         file_list.write('\n')
+print('wrote everything to file' + file_list.read())
 file_list.close()
+
